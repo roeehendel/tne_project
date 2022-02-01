6b405d76e2bb4ee05d460a9445cf80ddabe4483a
Karina Buchmann <karinabuchmann@gmail.com>
2022-02-01 18:40:06 +0200

    add to script tne_dataset.py : preposition_list, create_target

--- data_loading/tne_dataset.py
+++ data_loading/tne_dataset.py
@@ -1,9 +1,36 @@
 import gzip
 import json
 
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizer
+
+preposition_list = ['no-relation', 'of', 'against', 'in', 'by', 'on', 'about', 'with', 'after', 'member(s) of',
+                    'to', 'from', 'for', 'among', 'under', 'at', 'between', 'during', 'near', 'over', 'before',
+                    'inside', 'outside', 'into', 'around']
+
+"""
+:param
+len_nps - amount of nps in the example
+len_nps - list of dicts. relevant keys in each dict contains: 'anchor', 'complement', 'preposition' 
+          (there is 'complement_coref_cluster_id')
+"""
+
+
+def create_target(len_nps, np_relations, ignore_index=-100):
+    # creating target vec of k^2
+    target = np.zeros(len_nps ** 2)
+    # go over all the np relations and update target
+    for np_relation in np_relations:
+        preposition_index = preposition_list.index(np_relation['preposition'])
+        anchor_index = int(np_relation['anchor'][2:])
+        complement_index = int(np_relation['complement'][2:])
+        target[anchor_index * len_nps + complement_index] = preposition_index
+    # update target where anchor and complement are same to -100 to ignore them
+    indexes_anchor_complement_are_same = (len_nps + 1) * np.arange(len_nps)
+    target[indexes_anchor_complement_are_same] = -ignore_index
+    return target
 
 
 class TNEDataset(Dataset):
@@ -33,7 +60,7 @@ class TNEDataset(Dataset):
         mask = self.encoded_inputs['attention_mask'][idx]
         token_type_ids = self.encoded_inputs["token_type_ids"][idx]
 
-        target = len(item['nps']) > 35
+        target = create_target(len(item['nps']), item['np_relations'])
 
         return {
             'ids': torch.tensor(ids, dtype=torch.long),
