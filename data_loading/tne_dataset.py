79055e766a7ccc4c45f6841df65d2bf9a5bc6eca
Roee Hendel <roee97@gmail.com>
2022-02-01 18:01:42 +0200

    Move the tokenizer to init and start working on representing the nps

--- data_loading/tne_dataset.py
+++ data_loading/tne_dataset.py
@@ -7,17 +7,21 @@ from transformers import PreTrainedTokenizer
 
 
 class TNEDataset(Dataset):
-    def __init__(self, file_path: str, tokenizer: PreTrainedTokenizer, max_len: int):
+    def __init__(self, file_path: str, tokenizer: PreTrainedTokenizer, max_length: int):
+        self.tokenizer = tokenizer
+        self.max_len = max_length
+
         with gzip.open(file_path, 'rb') as f:
             lines = f.readlines()
 
         self.data = [json.loads(line) for line in lines]
 
         # TODO: remove this to use all data!!!
-        self.data = self.data[:256]
+        self.data = self.data[:16]
 
-        self.tokenizer = tokenizer
-        self.max_len = max_len
+        texts = [item['text'] for item in self.data]
+        self.encoded_inputs = self.tokenizer(texts, max_length=max_length, padding='max_length')
+        # TODO: use the char_to_token method of the tokenizer to get the token ids of the nps start and end
 
     def __len__(self):
         return len(self.data)
@@ -25,18 +29,9 @@ class TNEDataset(Dataset):
     def __getitem__(self, idx):
         item: dict = self.data[idx]
 
-        inputs = self.tokenizer.encode_plus(
-            item['text'],
-            None,
-            add_special_tokens=True,
-            max_length=self.max_len,
-            padding='max_length',
-            return_token_type_ids=True,
-            truncation=True
-        )
-        ids = inputs['input_ids']
-        mask = inputs['attention_mask']
-        token_type_ids = inputs["token_type_ids"]
+        ids = self.encoded_inputs['input_ids'][idx]
+        mask = self.encoded_inputs['attention_mask'][idx]
+        token_type_ids = self.encoded_inputs["token_type_ids"][idx]
 
         target = len(item['nps']) > 35
 
